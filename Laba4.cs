9769906a5cf0c703028595833083087f7fa6c760
VladimirShatrov <144910773+VladimirShatrov@users.noreply.github.com>
2024-11-19 17:56:13 +0500

    Laba4

---
+++ Laba4.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Serialization;
+
+public class Laba4
+{
+	public static List<T> DeleteRepeatingNumbers<T>(List<T> inputList)
+	{
+		List<T> outputList = new List<T> { inputList[0] };
+		for (int i = 1; i < inputList.Count; i++)
+		{
+			if (!EqualityComparer<T>.Default.Equals(inputList[i], inputList[i - 1]))
+			{
+				outputList.Add(inputList[i]);
+			}
+		}
+		return outputList;
+	}
+	
+	public static LinkedList<T> LinkedListFromList<T>(List<T> inputList)
+	{ 
+		LinkedList<T> outputList = new LinkedList<T>();
+
+		foreach (T item in inputList)
+		{
+			outputList.AddLast(item);
+		}
+		return outputList;
+	}
+
+	public static HashSet<T> AnyStudentsElective<T>(Dictionary<string, HashSet<T>> studentElectives)
+	{
+		HashSet<T> result = new HashSet<T>();
+
+        foreach (var elective in studentElectives.Values)
+        {
+			result.UnionWith(elective);
+        }
+		return result;
+    }
+	public static HashSet<T> AllStudentsElective<T>(Dictionary<string, HashSet<T>> studentsElective)
+	{
+		HashSet<T> result = new HashSet<T>(studentsElective.Values.First());
+		
+		foreach (var elective in studentsElective.Values)
+		{
+			result.IntersectWith(elective);
+		}
+
+		return result;
+	}
+	public static HashSet<T> NoStudentsElective<T>(HashSet<T> electives, Dictionary<string, HashSet<T>> studentsElectives)
+	{		
+		HashSet<T> anyStudentsElectives = AnyStudentsElective(studentsElectives);
+		electives.ExceptWith(anyStudentsElectives);
+		return electives;
+	}
+
+	private static List<string> Split(string input)
+	{
+		var result = new List<string>();
+		string word = "";
+		for (int i = 0; i < input.Length; i++)
+		{
+			if ((input[i] <= 1103) && (input[i] >= 1040))
+			{
+				word += input[i];
+			}
+			else
+			{
+				result.Add(word);
+				word = "";
+			}
+		}
+		result.Add(word);
+
+		return new List<string>(result.Where(word => !string.IsNullOrEmpty(word)));
+	}
+
+    private static List<string> SplitSpace(string input)
+    {
+        var result = new List<string>();
+        string word = "";
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] != ' ')
+            {
+                word += input[i];
+            }
+            else
+            {
+                result.Add(word);
+                word = "";
+            }
+        }
+        result.Add(word);
+
+        return new List<string>(result.Where(word => !string.IsNullOrEmpty(word)));
+    }
+
+    public static void LettersInWordsWithNotEvenNumbers(string path)
+	{
+		try
+		{
+			using (StreamReader file = new StreamReader(path))
+			{
+				HashSet<char> letters = new HashSet<char>();
+				string text = file.ReadToEnd();
+				if (string.IsNullOrEmpty(text))
+					return;
+
+				List<string> words = Split(text);
+				for (int i = 0; i < words.Count; i += 2)
+				{
+					for (int j = 0; j < words[i].Length; j++)
+						letters.Add(words[i].ToLower()[j]);
+				}
+
+				var list = new List<char>(letters);
+				list.Sort();
+				Console.WriteLine("В нечетных словах встречаются следующие символы: " + string.Join(", ", list));
+			}
+		}
+		catch (IOException e)
+		{
+			Console.WriteLine("Не удалось открыть файл: " + e.Message);
+		}
+	}
+
+	public static int WriteToFile(string path)
+	{
+		try
+		{
+			using (FileStream file = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+			{
+				XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<string>));
+				List<string> companies = new List<string> {"Компания1 Пушкина 92 1000", "Компания1 Пушкина 98 3000", "Компания2 Ленина 92 2222", "Компания3 Мира 92 1111", "Компания3 Мира 98 2000",
+					"Компания4 Куйбышева 92 1000", "Компания4 Куйбышева 98 2000", "Компания5 Коломенская 92 1001", "Компания5 Коломенская 98 2000", "Компания5 Пушкина 98 2000" };
+				int n = companies.Count;
+				xmlSerializer.Serialize(file, companies);
+				return n;
+			}
+		}
+		catch (IOException e)
+		{
+            Console.WriteLine("Не удалось открыть файл: " + e.Message);
+        }
+		return 0;
+	}
+
+	public static void GasStationsLessExpensiveGasoline(string path, int n)
+	{
+		try
+		{
+			if (n == 0)
+			{
+				Console.WriteLine("0 0 0");
+				return;
+			}
+
+			using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read))
+			{
+				XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<string>));
+				List<string> companies = (List<string>)xmlSerializer.Deserialize(file);
+
+				int lessExpensive92 = int.MaxValue, lessExpensive95 = int.MaxValue, lessExpensive98 = int.MaxValue;
+				int num92 = 0, num95 = 0, num98 = 0;
+				foreach (string gasStation in companies)
+				{
+					List<string> info = SplitSpace(gasStation);
+					switch (info[2])
+					{
+                        case "92": if (lessExpensive92 > int.Parse(info[3]))
+							{
+								lessExpensive92 = int.Parse(info[3]);
+								num92 = 1;
+							}
+							else if (lessExpensive92 == int.Parse(info[3]))
+								num92++;
+						break;
+						case "95": if (lessExpensive95 > int.Parse(info[3]))
+							{
+								lessExpensive95 = int.Parse(info[3]);
+								num95 = 1;
+							}
+							else if (lessExpensive95 == int.Parse(info[3]))
+								num95++;
+						break;
+						case "98": if (lessExpensive98 > int.Parse(info[3]))
+							{
+								lessExpensive98 = int.Parse(info[3]);
+								num98 = 1;
+							}
+							else if (lessExpensive98 == int.Parse(info[3]))
+								num98++;
+						break;
+                    }
+				}
+
+				Console.WriteLine("{0} {1} {2}", num92, num95, num98);
+            }
+		}
+        catch (IOException e)
+        {
+            Console.WriteLine("Не удалось открыть файл: " + e.Message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using static Laba4;
+
+public class Program
+{
+    private struct Point
+    {
+        private int x;
+        private int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("x: {0}, y: {1}", x, y);
+        }
+    }
+
+    private struct Elective
+    {
+        private string name;
+        
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Elective (string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Elective other)
+            {
+                return string.Equals(name, other.name);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            if (name == null)
+                return 0;
+            return name.GetHashCode();
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        //1
+        List<int> numList = new List<int> { 1, 1, 1, 2, 3, 1, 2, 3, 8, 0, 0, 1, 2, 99, 99, 1, 2, 3 };
+        List<Point> pointList = new List<Point> { new Point(1, 1), new Point(0, 0), new Point(0, 0) };
+        List<string> stringList = new List<string> { "aboba", "taa", "taa", "taa" };
+
+        numList = DeleteRepeatingNumbers(numList);
+        pointList = DeleteRepeatingNumbers(pointList);
+        stringList = DeleteRepeatingNumbers(stringList);
+
+        foreach (int i in numList)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+        foreach (Point i in pointList)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+        foreach (string i in stringList)
+        {
+            Console.WriteLine(i);
+        }
+
+        //2
+        LinkedList<int> linkedNumList = LinkedListFromList(numList);
+        LinkedList<Point> linkedPointList = LinkedListFromList(pointList);
+        LinkedList<string> linkedStringList = LinkedListFromList(stringList);
+        foreach (int i in linkedNumList)
+        {
+            Console.WriteLine("prev: {0}, value: {1}, next: {2}");
+        }
+        Console.WriteLine();
+        foreach (Point i in linkedPointList)
+        {
+            Console.WriteLine("prev: {0}, value: {1}, next: {2}");
+        }
+        Console.WriteLine();
+        foreach (string i in linkedStringList)
+        {
+            Console.WriteLine("prev: {0}, value: {1}, next: {2}");
+        }
+
+        //3
+        HashSet<string> electives = new HashSet<string> { "1", "2", "3", "4", "5", "6" };
+        HashSet<Elective> electivesObjects = new HashSet<Elective> { new Elective("математика"), new Elective("физика"),
+            new Elective("робототехника"), new Elective("не придумал"), new Elective("гооол") };
+
+        Dictionary<string, HashSet<string>> studentesElectives = new Dictionary<string, HashSet<string>>();
+        studentesElectives.Add("Иван", new HashSet<string> { "3", "2", "1" });
+        studentesElectives.Add("Максим", new HashSet<string> { "3" });
+        studentesElectives.Add("Петр", new HashSet<string> { "4", "3" });
+
+        Dictionary<string, HashSet<Elective>> studentesElectiveObj = new Dictionary<string, HashSet<Elective>>();
+        studentesElectiveObj.Add("Иван", new HashSet<Elective> { new Elective("математика"), new Elective("физика"), new Elective("робототехника") });
+        studentesElectiveObj.Add("Максим", new HashSet<Elective>{ new Elective("математика") });
+        studentesElectiveObj.Add("Петр", new HashSet<Elective> { new Elective("не придумал"), new Elective("математика") });
+
+        HashSet<string> allString = AllStudentsElective(studentesElectives);
+        HashSet<string> anyString = AnyStudentsElective(studentesElectives);
+        HashSet<string> noString = NoStudentsElective(electives, studentesElectives);
+
+        foreach (var i in allString) Console.WriteLine(i);
+        Console.WriteLine();
+        foreach (var i in anyString) Console.WriteLine(i);
+        Console.WriteLine();
+        foreach (var i in noString) Console.WriteLine(i);
+
+        Console.WriteLine();
+
+        HashSet<Elective> allElective = AllStudentsElective(studentesElectiveObj);
+        HashSet<Elective> anyElective = AnyStudentsElective(studentesElectiveObj);
+        HashSet<Elective> noElective = NoStudentsElective(electivesObjects, studentesElectiveObj);
+
+        foreach (var i in allElective) Console.WriteLine(i);
+        Console.WriteLine();
+        foreach (var i in anyElective) Console.WriteLine(i);
+        Console.WriteLine();
+        foreach (var i in noElective) Console.WriteLine(i);
+
+        Console.WriteLine();
+
+        //4
+        LettersInWordsWithNotEvenNumbers("notExistsFilePath.taa");
+        LettersInWordsWithNotEvenNumbers("1.txt");
+
+        //5
+        int n = WriteToFile("2.xml");
+        GasStationsLessExpensiveGasoline("notExistsFilePath.xml", n);
+        GasStationsLessExpensiveGasoline("2.xml", n);
+    }
+}
